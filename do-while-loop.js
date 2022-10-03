0caf6c5d08c7336837c0f1e8247fb8fd1a47b048
trinh-cong <111448704+trinh-cong@users.noreply.github.com>
2022-10-03 16:26:02 +0700

    d
    d

--- do-while-loop.js
+++
@@ -1,7 +0,0 @@
-var i=1
-var msg='';
-do{
-  msg+=i+'x5='+(i*5)+'<br/>';
-  i++;
-}while(i<=10);
-document.getElementById('answer').innerHTML=msg;
--- if-statement.js
+++
@@ -1,8 +0,0 @@
-var score=75;
-var msg;
-if(score>=50){
-  msg='Coinfratulations!';
-  msg+='Proceedto the next round.';
-}
-var el=document.getElementById('answer');
-el.textContent=msg;
